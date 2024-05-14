d5cf412a4d1be4ec58c0500bf9018e2f8e88352b
Genius <jamshidbekxaydarov6@gmail.com>
2024-05-15 03:36:07 +0500

    change settings.py and requirements.txt files

--- spotify_v1/settings.py
+++ spotify_v1/settings.py
@@ -1,5 +1,8 @@
 import os.path
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-72-*j#xhbsv1vih5u9l_xiofg%c5!*ap0h6dyjwn8ft@b@z^r5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-72-*j#xhbsv1vih5u9l_xiofg%c5!*ap0h6dyjwn8ft@b@z^r5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,11 +78,11 @@ WSGI_APPLICATION = 'spotify_v1.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "spotify_v1",
-        "USER": "postgres",
-        "PASSWORD": "password",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
